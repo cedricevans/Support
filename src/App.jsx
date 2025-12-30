aab598b08dea462a18f5e797a7ece5b55a710e0f
Cedric Evans <cedric.evans@gmail.com>
2025-12-30 08:50:29 -0500

    Use hash routing for direct navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
